3c5078c8d1bef4a401e41a9081ca12dc6645508b
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2021-05-23 13:24:46 +0530

    Update forms.py

--- donorproject/plasmadonor/forms.py
+++ donorproject/plasmadonor/forms.py
@@ -2,26 +2,43 @@ from django import forms
 from .models import Customer, Donor, requests,User
 
 
-
+Blood_choices = (
+    ("Don't know", "Don't know"),
+    ("O+", "O+"),
+    ("O-", "O-"),
+    ("A+", "A+"),
+    ("A-", "A-"),
+    ("B+", "B+"),
+    ("B-", "B-"),
+    ("AB+", "AB+"),
+    ("AB-", "AB-"),
+)
 class Donorform(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    slug = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    age = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    Blood_Group = forms.CharField(widget=forms.Select(choices=Blood_choices,attrs={'class':'form-control', 'placeholder':'Choose field'}))
     city = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    when_Recovered = forms.CharField(widget=forms.DateTimeInput(attrs={'class':'form-control'}))
+    when_Recovered = forms.DateField(widget=forms.DateTimeInput(attrs={'class':'form-control', 'placeholder':'YYYY-MM-DD'}))
     description = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
 
     class Meta:
         model = Donor
-        fields = ["name", "slug", "city", "when_Recovered", "description"]
+        fields = ["name", "age", "Blood_Group", "city", "when_Recovered", "description"]
         
-        
+situation_choices = (
+    ("Very Serious", "Very Serious"),
+    ("In Ventilator", "In Ventilator"),
+    ("Oxygen Below 50", "Oxygen Below 50"),
+    ("Not so serious", "Not so serious"),
+
+)        
         
 class CheckoutForm(forms.ModelForm):
     name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     city = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     mobile = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
-    email = forms.CharField(widget=forms.DateTimeInput(attrs={'class':'form-control'}))
-    currrent_situation = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    currrent_situation = forms.CharField(widget=forms.Select(choices=situation_choices,attrs={'class':'form-control', 'placeholder':'Choose field'}))
 
     class Meta:
         model = requests
