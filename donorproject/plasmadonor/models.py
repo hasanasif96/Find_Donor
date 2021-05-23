17293ad79fd3ab27414cae672da21d3371f73fb4
hasanasif96 <45166409+hasanasif96@users.noreply.github.com>
2021-05-23 13:24:07 +0530

    Update models.py

--- donorproject/plasmadonor/models.py
+++ donorproject/plasmadonor/models.py
@@ -14,15 +14,26 @@ class Customer(models.Model):
     def __str__(self):
         return self.full_name
 
-
+Blood_choices = (
+    ("Don't know", "Don't know"),
+    ("O+", "O+"),
+    ("O-", "O-"),
+    ("A+", "A+"),
+    ("A-", "A-"),
+    ("B+", "B+"),
+    ("B-", "B-"),
+    ("AB+", "AB+"),
+    ("AB-", "AB-"),
+)
 
 class Donor(models.Model):
     d_user = models.ForeignKey(Customer, on_delete=models.CASCADE)
     name = models.CharField(max_length=20)
-    slug = models.SlugField(unique=True)
+    age=models.CharField(max_length=4)
+    Blood_Group = models.CharField(max_length=50, choices=Blood_choices)
     city = models.CharField(max_length=20)
     when_Recovered= models.DateTimeField()
-    description = models.CharField(max_length=40)
+    description = models.CharField(max_length=80)
     
 
     def __str__(self):
@@ -37,7 +48,13 @@ ORDER_STATUS = (
     ("Request Cancelled", "Request Cancelled"),
 )
 
+situation_choices = (
+    ("Very Serious", "Very Serious"),
+    ("In Ventilator", "In Ventilator"),
+    ("Oxygen Below 50", "Oxygen Below 50"),
+    ("Not so serious", "Not so serious"),
 
+)
 
 class requests(models.Model):
     name=models.CharField(max_length=20)
@@ -46,7 +63,7 @@ class requests(models.Model):
     city = models.CharField(max_length=20)
     mobile = models.CharField(max_length=10)
     email = models.EmailField(null=True, blank=True)
-    currrent_situation = models.CharField(max_length=40)
+    currrent_situation = models.CharField(max_length=40, choices=situation_choices)
     created_at = models.DateTimeField(auto_now_add=True)
     order_status = models.CharField(max_length=50, choices=ORDER_STATUS, default="Request Pending")
     
